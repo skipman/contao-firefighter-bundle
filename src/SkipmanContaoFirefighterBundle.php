4ac2ce59e0bb921eb5b0440c0c36cd2df61060bb
Ronald Boda <7385395+skipman@users.noreply.github.com>
2022-07-28 17:22:12 +0200

    adding module ranks

---
+++ src/SkipmanContaoFirefighterBundle.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Firefighter Bundle.
+ * 
+ * (c) Ronald Boda 2022 <info@coboda.at>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/skipman/contao-firefighter-bundle
+ */
+
+namespace Skipman\ContaoFirefighterBundle;
+
+use Skipman\ContaoFirefighterBundle\DependencyInjection\SkipmanContaoFirefighterExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SkipmanContaoFirefighterBundle
+ */
+class SkipmanContaoFirefighterBundle extends Bundle
+{
+	public function getContainerExtension(): SkipmanContaoFirefighterExtension
+	{
+		return new SkipmanContaoFirefighterExtension();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function build(ContainerBuilder $container): void
+	{
+		parent::build($container);
+		
+	}
+}
